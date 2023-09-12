19dbca4c21d687439a97e668ea927e9242db571c
VadymSalymonenko <vadymsalymonenko1@gmail.com>
2023-09-12 18:25:24 +0300

    added new examples

--- STM32_AzureRTOS_Project/examples/hello_word_treadx/inc/stm32g0xx_it.h
+++ STM32_AzureRTOS_Project/examples/hello_word_treadx/inc/stm32g0xx_it.h
@@ -1,9 +1,8 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
+  * @file    stm32g0xx_it.h
+  * @brief   This file contains the headers of the interrupt handlers.
   ******************************************************************************
   * @attention
   *
@@ -14,21 +13,17 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
-  ******************************************************************************
+ ******************************************************************************
   */
-#include <string.h> 
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __STM32G0xx_IT_H
+#define __STM32G0xx_IT_H
 
 #ifdef __cplusplus
-extern "C" {
+ extern "C" {
 #endif
-
-/* Includes ------------------------------------------------------------------*/
-#include "stm32g0xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -51,20 +46,17 @@ extern "C" {
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
-void Error_Handler(void);
-
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void SVC_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __MAIN_H */
+#endif /* __STM32G0xx_IT_H */
--- STM32_AzureRTOS_Project/examples/hello_word_treadx/src/main.c
+++ STM32_AzureRTOS_Project/examples/hello_word_treadx/src/main.c
@@ -1,244 +1,555 @@
 /* USER CODE BEGIN Header */
+
 /**
+
   ******************************************************************************
+
   * @file           : main.c
+
   * @brief          : Main program body
+
   ******************************************************************************
+
   * @attention
+
   *
+
   * Copyright (c) 2023 STMicroelectronics.
+
   * All rights reserved.
+
   *
+
   * This software is licensed under terms that can be found in the LICENSE file
+
   * in the root directory of this software component.
+
   * If no LICENSE file comes with this software, it is provided AS-IS.
+
   *
+
   ******************************************************************************
+
   */
+
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
+
 #include "main.h"
 
+
+
 /* Private includes ----------------------------------------------------------*/
+
 /* USER CODE BEGIN Includes */
+
+
 
 /* USER CODE END Includes */
 
+
+
 /* Private typedef -----------------------------------------------------------*/
+
 /* USER CODE BEGIN PTD */
+
+
 
 /* USER CODE END PTD */
 
+
+
 /* Private define ------------------------------------------------------------*/
+
 /* USER CODE BEGIN PD */
+
 /* USER CODE END PD */
 
+
+
 /* Private macro -------------------------------------------------------------*/
+
 /* USER CODE BEGIN PM */
+
+
 
 /* USER CODE END PM */
 
+
+
 /* Private variables ---------------------------------------------------------*/
+
 UART_HandleTypeDef huart2;
+
+
 
 /* USER CODE BEGIN PV */
 
+void UART_Print(const char *str);
+
+
+
+TX_THREAD   hello_world_thread;
+
+UCHAR       hello_world_thread_stack[1024];
+
 /* USER CODE END PV */
 
+
+
 /* Private function prototypes -----------------------------------------------*/
+
 void SystemClock_Config(void);
+
 static void MX_GPIO_Init(void);
+
 static void MX_USART2_UART_Init(void);
+
+
+
+
+
 /* USER CODE BEGIN PFP */
+
+void ulong_to_str(unsigned long n, char *str) {
+
+    int i = 0, j = 0;
+
+    char temp[20]; 
+
+    do {
+
+        temp[i++] = (n % 10) + '0';
+
+        n = n / 10;
+
+    } while (n);
+
+    while (i--) {
+
+        str[j++] = temp[i];
+
+    }
+
+    str[j] = '\0';
+
+}
+
+
+
+void hello_world_thread_entry(ULONG thread_input) {
+
+    while (1) {
+
+        UART_Print("Hello world!!!\r\n");
+
+        tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 3); 
+
+    }
+
+}
+
+
+
+void tx_application_define(void *first_unused_memory)
+
+{
+
+    tx_thread_create(&hello_world_thread, "Hello world Thread",
+
+                    hello_world_thread_entry, 0,
+
+                    hello_world_thread_stack, sizeof(hello_world_thread_stack),
+
+                    1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+
+}
+
+
 
 /* USER CODE END PFP */
 
+
+
 /* Private user code ---------------------------------------------------------*/
+
 /* USER CODE BEGIN 0 */
+
 void UART_Print(const char *str) {
+
     HAL_UART_Transmit(&huart2, (uint8_t*)str, strlen(str), HAL_MAX_DELAY);
+
 }
+
 /* USER CODE END 0 */
 
+
+
 /**
+
   * @brief  The application entry point.
+
   * @retval int
+
   */
+
 int main(void)
+
 {
+
   /* USER CODE BEGIN 1 */
+
+
 
   /* USER CODE END 1 */
 
+
+
   /* MCU Configuration--------------------------------------------------------*/
 
+
+
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
   HAL_Init();
+
+
 
   /* USER CODE BEGIN Init */
 
+
+
   /* USER CODE END Init */
 
+
+
   /* Configure the system clock */
+
   SystemClock_Config();
+
+
 
   /* USER CODE BEGIN SysInit */
 
+
+
   /* USER CODE END SysInit */
 
+
+
   /* Initialize all configured peripherals */
+
   MX_GPIO_Init();
+
   MX_USART2_UART_Init();
+
+
   /* USER CODE BEGIN 2 */
+
+  tx_kernel_enter();
+
+
 
   /* USER CODE END 2 */
 
+
+
   /* Infinite loop */
+
   /* USER CODE BEGIN WHILE */
+
   while (1)
+
   {
-	    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);  // Зміна піну на PA5
-	    HAL_Delay(250);  // Затримка на 100 мілісекунд
-	    UART_Print("LED toggled\r\n");  // Вивід тексту через UART
+
+	   // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);  // Зміна піну на PA5
+
+	   // HAL_Delay(1000);  // Затримка на 100 мілісекунд
+
+	   // UART_Print("LED toggled\r\n");  // Вивід тексту через UART
+
     /* USER CODE END WHILE */
 
+
+
     /* USER CODE BEGIN 3 */
+
   }
+
   /* USER CODE END 3 */
+
 }
 
+
+
 /**
+
   * @brief System Clock Configuration
+
   * @retval None
+
   */
+
 void SystemClock_Config(void)
+
 {
+
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
+
+
   /** Configure the main internal regulator output voltage
+
   */
+
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
+
+
   /** Initializes the RCC Oscillators according to the specified parameters
+
   * in the RCC_OscInitTypeDef structure.
+
   */
+
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+
   RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+
   {
+
     Error_Handler();
+
   }
+
+
 
   /** Initializes the CPU, AHB and APB buses clocks
+
   */
+
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+
                               |RCC_CLOCKTYPE_PCLK1;
+
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
+
+
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+
   {
+
     Error_Handler();
+
   }
+
 }
 
+
+
 /**
+
   * @brief USART2 Initialization Function
+
   * @param None
+
   * @retval None
+
   */
+
 static void MX_USART2_UART_Init(void)
+
 {
+
+
 
   /* USER CODE BEGIN USART2_Init 0 */
 
+
+
   /* USER CODE END USART2_Init 0 */
+
+
 
   /* USER CODE BEGIN USART2_Init 1 */
 
+
+
   /* USER CODE END USART2_Init 1 */
+
   huart2.Instance = USART2;
+
   huart2.Init.BaudRate = 115200;
+
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
+
   huart2.Init.StopBits = UART_STOPBITS_1;
+
   huart2.Init.Parity = UART_PARITY_NONE;
+
   huart2.Init.Mode = UART_MODE_TX_RX;
+
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+
   huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+
   huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+
   huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
   if (HAL_UART_Init(&huart2) != HAL_OK)
+
   {
+
     Error_Handler();
+
   }
+
   /* USER CODE BEGIN USART2_Init 2 */
+
+
 
   /* USER CODE END USART2_Init 2 */
 
+
+
 }
+
+
 
 /**
+
   * @brief GPIO Initialization Function
+
   * @param None
+
   * @retval None
+
   */
+
 static void MX_GPIO_Init(void)
+
 {
+
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
+
+
   /* GPIO Ports Clock Enable */
+
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
+
+
   /*Configure GPIO pin Output Level */
+
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 
+
+
   /*Configure GPIO pin : PA5 */
+
   GPIO_InitStruct.Pin = GPIO_PIN_5;
+
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+
+
 }
+
+
 
 /* USER CODE BEGIN 4 */
 
+
+
 /* USER CODE END 4 */
 
+
+
 /**
+
   * @brief  This function is executed in case of error occurrence.
+
   * @retval None
+
   */
+
 void Error_Handler(void)
+
 {
+
   /* USER CODE BEGIN Error_Handler_Debug */
+
   /* User can add his own implementation to report the HAL error return state */
+
   __disable_irq();
+
   while (1)
+
   {
+
   }
+
   /* USER CODE END Error_Handler_Debug */
+
 }
 
+
+
 #ifdef  USE_FULL_ASSERT
+
 /**
+
   * @brief  Reports the name of the source file and the source line number
+
   *         where the assert_param error has occurred.
+
   * @param  file: pointer to the source file name
+
   * @param  line: assert_param error line source number
+
   * @retval None
+
   */
+
 void assert_failed(uint8_t *file, uint32_t line)
+
 {
+
   /* USER CODE BEGIN 6 */
+
   /* User can add his own implementation to report the file name and line number,
+
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
   /* USER CODE END 6 */
+
 }
+
 #endif /* USE_FULL_ASSERT */
+
